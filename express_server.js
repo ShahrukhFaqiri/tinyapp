52992762351304618ef96ab5a633b37afc07c3d0
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-22 23:41:46 +0000

    Added delete functionality using post

--- express_server.js
+++ express_server.js
@@ -36,12 +36,11 @@ app.get('/urls/new', (req, res) => {
 app.post('/urls', (req, res) => {
   const randString = generateRandomString();
   urlDatabase[randString] = req.body.longURL;
-  console.log("Database:",urlDatabase)
-  console.log("Parser:",req.body);
-  res.send('Ok');
+  res.redirect(`/urls`)
+
 });
 
-app.get('/u/:shortURL', (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
@@ -50,6 +49,12 @@ app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL)
 });
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect(`/urls`)
+})
 
 app.get('/hello', (req, res) => {
   res.send(`<html><body>Hello <b>World</b></body></html>\n`);
