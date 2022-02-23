01191f7d1b73197e94185ee73c9502cd687b845e
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-23 02:26:54 +0000

    Added the editing ability of already added links

--- express_server.js
+++ express_server.js
@@ -62,6 +62,13 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect(`/urls`);
 });
 
+//EDITING EXISTING LINK
+app.post('/urls/:id', (req, res) => {
+  const shortURL = req.params.id;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect('/urls')
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
